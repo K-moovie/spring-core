cb7f3997b084486d31380895467b785d5ddea669
K-moovie <kyh97@naver.com>
2021-02-01 22:35:53 +0900

    회원 도메인 실행과 테스트

---
+++ src/main/java/hello/springcore/member/MemberApp.java
@@ -0,0 +1,15 @@
+package hello.springcore.member;
+
+public class MemberApp {
+
+    // Application을 main method를 통해 test하는 것은 바람직하지 않다.
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println("new Member = " + member.getName());
+        System.out.println("find Member = " + findMember.getName());
+    }
+}

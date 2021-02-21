517bbdd2ce63c50e7564bd6b42f233910ee8b9a1
K-moovie <kyh97@naver.com>
2021-02-21 18:40:01 +0900

    Annotation 직접 만들기

--- src/main/java/hello/springcore/order/OrderServiceImpl.java
+++ src/main/java/hello/springcore/order/OrderServiceImpl.java
@@ -1,5 +1,6 @@
 package hello.springcore.order;
 
+import hello.springcore.annotation.MainDiscountPolicy;
 import hello.springcore.discount.DiscountPolicy;
 import hello.springcore.member.Member;
 import hello.springcore.member.MemberRepository;
@@ -17,7 +18,7 @@ public class OrderServiceImpl implements OrderService{
     private final DiscountPolicy discountPolicy;
 
     @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+    public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
